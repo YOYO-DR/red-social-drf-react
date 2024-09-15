83a81ef655d4ad6dcf9256e7d07e9a048ebcf01b
Yoiner Duran <yoiner3216898182@gmail.com>
2024-09-15 14:54:31 -0500

    avanzando

--- django-api/config/db.py
+++ django-api/config/db.py
@@ -1,8 +1,14 @@
 import os
+<<<<<<< HEAD
 from config.settings import BASE_DIR
 
 DataBasesConfig = {
   "a": {
+=======
+
+DataBasesConfig = {
+  "default": {
+>>>>>>> dd2a5dc (avanzando)
     "ENGINE": "django.db.backends.postgresql",
     "NAME": os.getenv("DBPOSNAME"),
     "USER": os.getenv("DBPOSUSER"),
@@ -17,9 +23,13 @@ DataBasesConfig = {
       "PASSWORD": "root",
       "HOST": "localhost",
       "PORT": "5432",
+<<<<<<< HEAD
     },
   "default": {
     "ENGINE": "django.db.backends.sqlite3",
     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
   },
+=======
+    }
+>>>>>>> dd2a5dc (avanzando)
 }
